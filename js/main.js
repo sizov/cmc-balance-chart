1786402652af0c73077fda7a90aed9d8e7173bda
Vladimir Sizov <sizov.v.f@gmail.com>
2015-03-08 19:50:43 +0000

    Adding file upload

--- js/main.js
+++ js/main.js
@@ -1,18 +1,54 @@
 $(document).ready(function () {
-
-    /**
-     * proper timeformat for cmc markets CSV: moment('07.03.2015 22:36:36,100', 'DD-MM-YYYY HH:mm:ss,SSS')
-     */
-
     var CMC_TIME_FORMAT = 'DD-MM-YYYY HH:mm:ss',
         CMC_DATE_HEADER = 'Date / Time',
         CMC_BALANCE_HEADER = 'Balance(GBP)',
-        balanceSeriesData = [],
-        CSV_LINK = 'data/History.csv',// 'data-cmc.csv';
         HEADER_SEPARATOR = ',',
         CELLS_SEPARATOR = '\",\"',
         CHART_DOM_SELECTOR = '.chart';
 
+    var balanceSeriesData = [],
+        highchartsOptions = {
+            title: {
+                text: 'CMC Account Balance'
+            },
+            subtitle: {
+                text: 'Chart of account balance based on CMC Markets exported CSV data'
+            },
+            xAxis: {
+                type: 'datetime',
+                title: {
+                    text: CMC_DATE_HEADER
+                }
+            },
+            yAxis: {
+                title: {
+                    text: CMC_BALANCE_HEADER
+                },
+                min: 0
+            },
+            tooltip: {
+                headerFormat: '<b>{point.y:.2f}</b><br>',
+                pointFormat: '{point.x:%A, %b %e, %H:%M:%S}'
+            },
+
+            plotOptions: {
+                series: {
+                    animation: false
+                },
+                spline: {
+                    marker: {
+                        enabled: true
+                    }
+                }
+            },
+
+            series: [
+                {
+                    name: CMC_BALANCE_HEADER,
+                    data: balanceSeriesData
+                }
+            ]
+        };
 
     function parseDate(dateString) {
         var parsed = moment(dateString, CMC_TIME_FORMAT);
@@ -39,52 +75,19 @@ $(document).ready(function () {
         return string;
     }
 
+    function compareDates(dateA, dateB) {
+        if (dateA === dateB) {
+            return 0;
+        }
 
-    var highchartsOptions = {
-        title: {
-            text: 'CMC Account Balance'
-        },
-        subtitle: {
-            text: 'Chart of account balance based on CMC Markets exported CSV data'
-        },
-        xAxis: {
-            type: 'datetime',
-            title: {
-                text: CMC_DATE_HEADER
-            }
-        },
-        yAxis: {
-            title: {
-                text: CMC_BALANCE_HEADER
-            },
-            min: 0
-        },
-        tooltip: {
-            headerFormat: '<b>{point.y:.2f}</b><br>',
-            pointFormat: '{point.x:%A, %b %e, %H:%M:%S}'
-        },
+        if (dateA < dateB) {
+            return -1;
+        }
 
-        plotOptions: {
-            series: {
-                animation: false
-            },
-            spline: {
-                marker: {
-                    enabled: true
-                }
-            }
-        },
+        return 1;
+    }
 
-        series: [
-            {
-                name: CMC_BALANCE_HEADER,
-                data: balanceSeriesData
-            }
-        ]
-    };
-
-
-    $.get(CSV_LINK, function (csv) {
+    function processCsvFile(csv) {
         // Split the lines
         var lines = csv.split('\n');
 
@@ -130,8 +133,36 @@ $(document).ready(function () {
 
         });
 
-        $(CHART_DOM_SELECTOR).highcharts(highchartsOptions);
-    });
+        //sort array, we know that feach element is Date/Balance pair, so let's sort by date
+        balanceSeriesData.sort(function (a, b) {
+            return compareDates(a[0], b[0])
+        });
 
+        $(CHART_DOM_SELECTOR).highcharts(highchartsOptions);
+    }
+
+    //uncomment if you want to use local demo file
+//    var CSV_LINK = 'data/History.csv';
+//    $.get(CSV_LINK, processCsvFile);
+
+    function handleFileSelect(evt) {
+        var files = evt.target.files,
+            file = files[0],
+            reader = new FileReader();
+
+        reader.onload = function (e) {
+            var fileContent = e.target.result;
+            processCsvFile(fileContent);
+        };
+
+        // Read in the file
+        reader.readAsText(file);
+    }
+
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        document.getElementById('file').addEventListener('change', handleFileSelect, false);
+    } else {
+        alert('The File APIs are not fully supported in this browser.');
+    }
 
 });
